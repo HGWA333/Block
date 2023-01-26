ffea61d33e7d095d6ba65b9d634dd3e26e4563f0
wg333 <88034910+HGWA333@users.noreply.github.com>
2023-01-26 17:08:49 +0900

    hi

--- TypeScript/06.Generics/index.ts
+++ TypeScript/06.Generics/index.ts
@@ -1,89 +1,83 @@
-// Typescript 자료형
+// Typescript 제네릭(Generics) 타입
 
-let num: number = 1234;
-let str: string = "1234";
-let bool: boolean = true;
-let und: undefined = undefined;
-let nul: null = null;
-
-// und = 1234 으로 초기화시 에러가 난다.
-// Typescript는 자료형(Type)을 확인하기 때문에 같은 자료형만 변수에 정의할 수 있다.
-
-num = 4321;
-// console.log(num.length); number 에는 길이가 없기 때문에 오류가 난다.
-// console.log(str.length); string 에는 길이가 있기 때문에 오류가 안난다.
-
-let any: any = undefined;
-any = "1234";
-any = 1234;
-// any는 모든 자료형을 포함한다. 그래서 웬만해서는 사용 안하는 것이 좋다.
-// any를 사용하는 것은 기존 Javascript를 사용하는 것 과 같음
-
-let unk: unknown = undefined;
-unk = "1234";
-
-console.log(any.length); // 길이가 존재 (모든 자료형을 포함하고 있다고 판단하여 각종 메서드, 키를 모두 사용할 수 있다.)
-console.log(unk.length); // 길이가 존재 하지 않음 (자료형을 모른다고 판단하여 각종 메서드, 키를 사용할 수 없다. unk은 Type을 확인 후에 메서드, 키를 사용할 수 있음)
-
-if (typeof unk == "string") {
-  // unk은 Type을 확인 후에 해당 타입에 대한 메서드, 키를 사용할 수 있다.
-  console.log(unk.length);
+function TestGetSize(arr: number[] | string[] | boolean[] | object[]): number {
+  return arr.length;
 }
 
-let objNum: { a: number } = { a: 1 };
-let objStr: { a: string } = { a: "1" };
+const TestArr1 = [1, 2, 3];
+TestGetSize(TestArr1);
 
-let obj: { a: number; b?: string } = { a: 1 };
-// b뒤에 붙은 ?는 string 뿐만 아니라 undefined를 포함한다.
+const TestArr2 = ["a", "b", "c"];
+TestGetSize(TestArr2);
 
-let numUnd: number | undefined = undefined;
-// |를 사용하여 type를 여러 개 사용할 수 있다.
-// 앞의 자료형이 없으면 뒤에 위치한 자료형을 적용한다.
+const TestArr3 = [false, true, true];
+TestGetSize(TestArr3);
 
-let objTest: { a: number; b?: string } = { a: 1 };
-// ?는 undefined를 포함한다.
-obj.b = "1234";
+const TestArr4 = [{}, {}, { name: "trim" }];
+TestGetSize(TestArr4);
 
-let arr: Array<number> = [1, 2, 3];
-// arr.push("abcd");
-// Array 배열은 number형으로 설정을 해 놓아서 arr.push "abcd" string형을 넣을 수 없다.
+// --- 제네릭 타입1
 
-let arr1: [number, string] = [1, "1"];
-// arr1.push(1)
-
-let arr2: string[] = ["1", "a", "b"];
-// arr1.push("abc")
-
-let arr3: Array<any> = [undefined, null, 1, "abc"];
-// 웬만해선 사용하지 말 것
-
-function funcA(): void {
-  // return이 없기 때문에 void type을 사용한다.
-  // 리턴이 없을 때 사용
-  console.log("funcA");
+function getSize<T>(arr: T[]): number {
+  return arr.length;
 }
 
-const funcB = function (): number {
-  return 1;
-  // 만약 number 타입에서 return이 없으면 에러가 난다.
+const arr1 = [1, 2, 3];
+getSize<number | string>(arr1);
+
+const arr2 = ["a", "b", "c"];
+getSize<string>(arr2);
+
+const arr3 = [false, true, true];
+getSize<boolean>(arr3);
+
+const arr4 = [{}, {}, { name: "trim" }];
+getSize<object>(arr4);
+
+// --- 제네릭 타입2
+
+interface TestMobile<T> {
+  name: string;
+  option: T;
+}
+
+const m1: TestMobile<{ color: string; coupon: boolean }> = {
+  name: "iphone12",
+  option: {
+    color: "blue",
+    coupon: false,
+  },
 };
 
-const funcC = function (): string {
-  return "abc";
-  // 만약 string 타입에서 return이 없으면 에러가 난다.
+const m2: TestMobile<string> = {
+  name: "iphone15",
+  option: "yeah",
 };
 
-// 함수의 return에 대한 type은 () 뒤에 붙인다.
+// --- 제네릭 타입3
 
-function sumA(a: number, b: number): number {
-  return a + b;
-}
-// 함수 뿐만 아니라 매개변수에도 타입을 붙혀준다.
-
-function sumB(numbers: { a: number; b: number }): number {
-  return numbers.a + numbers.b;
+interface User {
+  name: string;
+  age: number;
 }
 
-const sumC = ({ a, b }: { a: number; b: number }): number => {
-  return a + b;
-};
+interface MyCar {
+  name: string;
+  color: string;
+}
+
+interface Book {
+  price: number;
+}
+
+const heyman: User = { name: "hi", age: 11 };
+const car: MyCar = { name: "n74", color: "green" };
+const book: Book = { price: 3000 };
+
+function showName<T extends { name: string }>(_data: T): string {
+  return _data.name;
+}
+
+showName(heyman);
+showName(car);
+// showName(book);
--- TypeScript/07.Utily/index.ts
+++ TypeScript/07.Utily/index.ts
@@ -1,89 +1,122 @@
-// Typescript 자료형
+// Typescript 유틸리티(Utility) 타입
 
-let num: number = 1234;
-let str: string = "1234";
-let bool: boolean = true;
-let und: undefined = undefined;
-let nul: null = null;
+// ----- keyof
 
-// und = 1234 으로 초기화시 에러가 난다.
-// Typescript는 자료형(Type)을 확인하기 때문에 같은 자료형만 변수에 정의할 수 있다.
-
-num = 4321;
-// console.log(num.length); number 에는 길이가 없기 때문에 오류가 난다.
-// console.log(str.length); string 에는 길이가 있기 때문에 오류가 안난다.
-
-let any: any = undefined;
-any = "1234";
-any = 1234;
-// any는 모든 자료형을 포함한다. 그래서 웬만해서는 사용 안하는 것이 좋다.
-// any를 사용하는 것은 기존 Javascript를 사용하는 것 과 같음
-
-let unk: unknown = undefined;
-unk = "1234";
-
-console.log(any.length); // 길이가 존재 (모든 자료형을 포함하고 있다고 판단하여 각종 메서드, 키를 모두 사용할 수 있다.)
-console.log(unk.length); // 길이가 존재 하지 않음 (자료형을 모른다고 판단하여 각종 메서드, 키를 사용할 수 없다. unk은 Type을 확인 후에 메서드, 키를 사용할 수 있음)
-
-if (typeof unk == "string") {
-  // unk은 Type을 확인 후에 해당 타입에 대한 메서드, 키를 사용할 수 있다.
-  console.log(unk.length);
+interface KeyUser {
+  id: number;
+  gender: "m" | "f";
 }
 
-let objNum: { a: number } = { a: 1 };
-let objStr: { a: string } = { a: "1" };
+type UserKey = keyof User;
 
-let obj: { a: number; b?: string } = { a: 1 };
-// b뒤에 붙은 ?는 string 뿐만 아니라 undefined를 포함한다.
+const uk: UserKey = "age";
 
-let numUnd: number | undefined = undefined;
-// |를 사용하여 type를 여러 개 사용할 수 있다.
-// 앞의 자료형이 없으면 뒤에 위치한 자료형을 적용한다.
+// ----- Partial<t>
 
-let objTest: { a: number; b?: string } = { a: 1 };
-// ?는 undefined를 포함한다.
-obj.b = "1234";
-
-let arr: Array<number> = [1, 2, 3];
-// arr.push("abcd");
-// Array 배열은 number형으로 설정을 해 놓아서 arr.push "abcd" string형을 넣을 수 없다.
-
-let arr1: [number, string] = [1, "1"];
-// arr1.push(1)
-
-let arr2: string[] = ["1", "a", "b"];
-// arr1.push("abc")
-
-let arr3: Array<any> = [undefined, null, 1, "abc"];
-// 웬만해선 사용하지 말 것
-
-function funcA(): void {
-  // return이 없기 때문에 void type을 사용한다.
-  // 리턴이 없을 때 사용
-  console.log("funcA");
+interface PartialUser {
+  id: number;
+  gender: "m" | "f";
 }
 
-const funcB = function (): number {
-  return 1;
-  // 만약 number 타입에서 return이 없으면 에러가 난다.
+let PartialAdmin: Partial<PartialUser> = {
+  id: 1,
 };
 
-const funcC = function (): string {
-  return "abc";
-  // 만약 string 타입에서 return이 없으면 에러가 난다.
-};
+// ----- Required<t>
 
-// 함수의 return에 대한 type은 () 뒤에 붙인다.
-
-function sumA(a: number, b: number): number {
-  return a + b;
-}
-// 함수 뿐만 아니라 매개변수에도 타입을 붙혀준다.
-
-function sumB(numbers: { a: number; b: number }): number {
-  return numbers.a + numbers.b;
+interface RequiredUser {
+  id: number;
+  gender: "m" | "f";
 }
 
-const sumC = ({ a, b }: { a: number; b: number }): number => {
-  return a + b;
+let Required: Required<RequiredUser> = {
+  id: 1,
+  gender: "m",
 };
+
+// ----- Readonly<t>
+interface ReadonlyUser {
+  id: number;
+  gender: "m" | "f";
+}
+
+let ReadonlyAdmin: Readonly<ReadonlyUser> = {
+  id: 1,
+  gender: "m",
+};
+
+ReadonlyAdmin.id = 4;
+
+// ----- Record<K, T>
+
+interface TestScore {
+  "1": "A" | "B" | "C" | "D";
+  "2": "A" | "B" | "C" | "D";
+  "3": "A" | "B" | "C" | "D";
+  "4": "A" | "B" | "C" | "D";
+}
+
+const Testsocre: TestScore = {
+  1: "A",
+  2: "C",
+  3: "B",
+  4: "D",
+};
+
+type Grade = "1" | "2" | "3" | "4";
+type Score = "A" | "B" | "C" | "D";
+const socre: Record<Grade, Score> = {
+  1: "A",
+  2: "C",
+  3: "B",
+  4: "D",
+};
+
+interface RecordUser {
+  id: number;
+  name: string;
+}
+
+function isValid(_user: RecordUser) {
+  const result: Record<keyof RecordUser, boolean> = {
+    id: _user.id > 0,
+    name: _user.name !== "",
+  };
+  return result;
+}
+
+// ----- Pick<T, K>
+
+interface PickUser {
+  id: number;
+  name: string;
+}
+
+const PickAdmin: Pick<PickUser, "id" | "name"> = {
+  id: 9,
+  name: "hey",
+};
+
+// ----- Omit<T, K>
+
+interface OmitUser {
+  id: number;
+  name: string;
+  age: number;
+  gender: "m" | "f";
+}
+
+const OmitAdmin: Omit<OmitUser, "age" | "gender"> = {
+  id: 9,
+  name: "hey",
+};
+
+// ----- Exclude<T1, T2>
+
+type T1 = string | number | boolean;
+type T2 = Exclude<T1, number | string>;
+
+// ----- NonNullable<Type>
+
+type N1 = string | null | undefined | void;
+type N2 = NonNullable<N1>;
